f02a55c75de44240aeb060ae48fc603848127664
Andrew turner <andrew@andrew-turner.co.uk>
2018-02-28 13:54:23 +0000

    more mods, but no data in blocks

--- blockchain/block.js
+++ blockchain/block.js
@@ -17,11 +17,11 @@ class Block {
   }
 
   static genesis() {
-    return new this('Genesis time', '-----', 'f1r57-h45h',[]);
+    return new this('Genesis time', '-----', 'f1r57-h45h', []);
 
   }
 
-  static mineBlock(lastBlock, data){
+  static mineBlock(lastBlock, data) {
     const timestamp = Date.now();
     const lastHash = lastBlock.hash;
     const hash = Block.hash(timestamp,lastHash, data);
